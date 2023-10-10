b7a2a68b8b6321720d31982f50911cc6ec2d1841
Gaston Pallas <pallasgaston@gmail.com>
2023-10-10 14:06:05 +0200

    init commit

---
+++ src/main/java/fr/workshop/respectboundsbackend/RespectBoundsBackEndApplication.java
@@ -0,0 +1,13 @@
+package fr.workshop.respectboundsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespectBoundsBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RespectBoundsBackEndApplication.class, args);
+	}
+
+}
